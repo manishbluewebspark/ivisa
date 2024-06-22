776e335c4cc4a2925932a3f5f7908c62c05a1a05
sandeep yadav <sandeep@sorttech.ca>
2024-06-21 20:47:43 -0400

    nav new agin

--- src/App.js
+++ src/App.js
@@ -15,14 +15,8 @@ import { ToastContainer } from 'react-toastify';
 import SingleEntryFortenDay from './Components/Entryvisa/SingleEntry/SingleEntryFortenDay';
 import SingleEntry_30day  from './Components/Entryvisa/SingleEntry/SingleEntry_30day';
 import SingleEntrySixty  from './Components/Entryvisa/SingleEntry/SingleEntrySixty';
-
 import SingleEntry_30dayGcc  from './Components/Entryvisa/SingleEntry/SingleEntry_30dayGcc';
-
 import SingleEntryNinetySixhour  from './Components/Entryvisa/SingleEntry/SingleEntryNinetySixhour';
-
-
-
-
 import MultiEntry_30day  from './Components/Entryvisa/MultiEntry/MultiEntry_30day';
 import MultiEntry_60day from './Components/Entryvisa/MultiEntry/MultiEntry_60day'; 
 
@@ -40,20 +34,13 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/contact-us" element={<ContactUs />}></Route>
           <Route path="/apply" element={<ApplyForm />}></Route>
-
-
-
           <Route path="/SingleEntryFortenDay" element={<SingleEntryFortenDay />}></Route>
           <Route path="/SingleEntry_30day" element={<SingleEntry_30day />}></Route>
-          <Route path="/SingleEntry_30dayGcc" element={<SingleEntry_30dayGcc />}></Route>
-          <Route path="/SingleEntryFortenDay" element={<SingleEntryFortenDay />}></Route>
-          <Route path="/SingleEntryNinetySixhour" element={<SingleEntryNinetySixhour />}></Route>
           <Route path="/SingleEntrySixty" element={<SingleEntrySixty />}></Route>
+          <Route path="/SingleEntry_30dayGcc" element={<SingleEntry_30dayGcc />}></Route>
+          <Route path="/SingleEntryNinetySixhour" element={<SingleEntryNinetySixhour />}></Route>
           <Route path="/MultiEntry_30day" element={<MultiEntry_30day />}></Route>
           <Route path="/MultiEntry_60day" element={<MultiEntry_60day />}></Route>
-
-
-          
         </Routes>
         <ToastContainer />
       </BrowserRouter>
