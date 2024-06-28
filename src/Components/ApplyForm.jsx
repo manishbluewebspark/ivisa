30ec1b763f7b69837d33515f994a330a67dc65f0
sandeep yadav <sandeep@sorttech.ca>
2024-06-28 17:42:50 -0400

    Update minor

--- src/Components/ApplyForm.jsx
+++ src/Components/ApplyForm.jsx
@@ -119,146 +119,169 @@ const ApplyForm = () => {
     <>
       <Header2 title={title}></Header2>
       <div className="container my-5">
-        <div className="row justify-content-center">
-          <div className="col-lg-10">
-            <div className="card shadow-sm">
-              <div className="card-body">
-                <form onSubmit={handleSubmit}>
-                  <h2 className="mb-4">Basic Information</h2>
-                  <hr/>
-                  <div>
-                    <label>Visa Type :</label>
-                    <h3> Entry {location.state.visa.type} Time  - {location.state.visa.duration} {location.state.visa.visa_type} - {location.state.visa.service_type} 
-                      (AED {location.state.visa.price})
-                    </h3>
-                  </div>
-                  <div className="row mb-4">
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Title</label>
-                      <select className="form-select" name="title" value={formData.title} onChange={handleChange} required>
-                        <option value="">Select Title</option>
-                        <option value="Mr">Mr</option>
-                        <option value="Ms">Ms</option>
-                        <option value="Mrs">Mrs</option>
-                      </select>
-                    </div>
+  <div className="row justify-content-center">
+    <div className="col-lg-10">
+      <div className="card shadow-sm">
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
 
-                    <input type="hidden" name="id" onChange={handleChange} value={location.state.visa.id}/>
-                    <input type="hidden" name="price" onChange={handleChange} value={location.state.visa.price}/>
-                    <input type="hidden" name="duration" onChange={handleChange} value={location.state.visa.duration} />
-                    <input type="hidden" name="visa_type" onChange={handleChange} value={location.state.visa.visa_type} />
-                    <input type="hidden" name="service_type" onChange={handleChange} value={location.state.visa.service_type} />
+            <h2 className="mb-4">Basic Information</h2>
+            <hr/>
 
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">First Name</label>
-                      <input type="text" className="form-control" name="firstName" value={formData.firstName} onChange={handleChange} required />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Middle Name</label>
-                      <input type="text" className="form-control" name="middleName" value={formData.middleName} onChange={handleChange} />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Last Name</label>
-                      <input type="text" className="form-control" name="lastName" value={formData.lastName} onChange={handleChange} required />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Email</label>
-                      <input type="email" className="form-control" name="email" value={formData.email} onChange={handleChange} required />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Mobile Number with Country Code</label>
-                      <div className="input-group">
-                        <select className="form-select" name="countryCode" value={formData.countryCode} onChange={handleChange} required>
-                          <option value="+91">🇮🇳 India +91</option>
-                          <option value="+1">🇺🇸 USA +1</option>
-                          <option value="+1">🇨🇦 Canada +1</option>
-                          <option value="+44">🇬🇧 UK +44</option>
-                        </select>
-                        <input type="tel" className="form-control" name="mobileNumber" value={formData.mobileNumber} onChange={handleChange} required />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="row mb-4">
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Gender</label>
-                      <select className="form-select" name="gender" value={formData.gender} onChange={handleChange} required>
-                        <option value="">Select Gender</option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
-                      </select>
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Date of Birth</label>
-                      <input type="date" className="form-control" name="dob" value={formData.dob} onChange={handleChange} required />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Nationality</label>
-                      <input type="text" className="form-control" name="nationality" value={formData.nationality} onChange={handleChange} required />
-                    </div>
-                  </div>
-
-                  <h2>Passport Information</h2>
-                  <hr/>
-                  <div className="row mb-4">
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Passport Number</label>
-                      <input type="text" className="form-control" name="passportNumber" value={formData.passportNumber} onChange={handleChange} required />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Passport Issued Date</label>
-                      <input type="date" className="form-control" name="passportIssuedDate" value={formData.passportIssuedDate} onChange={handleChange} />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Passport Expiry Date</label>
-                      <input type="date" className="form-control" name="passportExpiryDate" value={formData.passportExpiryDate} onChange={handleChange} required />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Passport Issued Country</label>
-                      <select className="form-select" name="passportIssuedCountry" value={formData.passportIssuedCountry} onChange={handleChange} required>
-                        <option value="">Select Country</option>
-                        {topCountries.map((country) => (
-                          <option key={country} value={country}>{country}</option>
-                        ))}
-                      </select>
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Country of Residence</label>
-                      <select className="form-select" name="countryOfResidence" value={formData.countryOfResidence} onChange={handleChange} required>
-                        <option value="">Select Country</option>
-                        {topCountries.map((country) => (
-                          <option key={country} value={country}>{country}</option>
-                        ))}
-                      </select>
-                    </div>
-                  </div>
-
-                  <h2>Documents Upload</h2>
-                  <hr/>
-                  <div className="row mb-4">
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Utility Bill</label>
-                      <input type="file" className="form-control" name="utilityBill" onChange={handleChange} multiple />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Photo</label>
-                      <input type="file" className="form-control" name="photo" onChange={handleChange} multiple />
-                    </div>
-                    <div className="col-md-4 mb-3">
-                      <label className="form-label">Passport Scan</label>
-                      <input type="file" className="form-control" name="passportScan" onChange={handleChange} multiple />
-                    </div>
-                  </div>
-
-                  <div className="d-grid gap-2">
-                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                  </div>
-                </form>
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Visa Type:</label>
+              <div className="col-sm-10">
+                <h3>Entry {location.state.visa.type} Time - {location.state.visa.duration} {location.state.visa.visa_type} - {location.state.visa.service_type} (AED {location.state.visa.price})</h3>
               </div>
             </div>
-          </div>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Name</label>
+              <div className="col-sm-2">
+                <label className="col-form-label">Title</label>
+                <select className="form-select" name="title" value={formData.title} onChange={handleChange} required>
+                  <option value="">Select Title</option>
+                  <option value="Mr">Mr</option>
+                  <option value="Ms">Ms</option>
+                  <option value="Mrs">Mrs</option>
+                </select>
+              </div>
+              <div className="col-sm-2">
+                <label className="col-form-label">First Name</label>
+                <input type="text" className="form-control" name="firstName" value={formData.firstName} onChange={handleChange} required />
+              </div>
+              <div className="col-sm-2">
+                <label className="col-form-label">Middle Name</label>
+                <input type="text" className="form-control" name="middleName" value={formData.middleName} onChange={handleChange} />
+              </div>
+              <div className="col-sm-2">
+                <label className="col-form-label">Last Name</label>
+                <input type="text" className="form-control" name="lastName" value={formData.lastName} onChange={handleChange} required />
+              </div>
+            </div>
+
+            <input type="hidden" name="id" onChange={handleChange} value={location.state.visa.id}/>
+            <input type="hidden" name="price" onChange={handleChange} value={location.state.visa.price}/>
+            <input type="hidden" name="duration" onChange={handleChange} value={location.state.visa.duration} />
+            <input type="hidden" name="visa_type" onChange={handleChange} value={location.state.visa.visa_type} />
+            <input type="hidden" name="service_type" onChange={handleChange} value={location.state.visa.service_type} />
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Email</label>
+              <div className="col-sm-10">
+                <input type="email" className="form-control" name="email" value={formData.email} onChange={handleChange} required />
+              </div>
+            </div>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Mobile Number with Country Code</label>
+              <div className="col-sm-10">
+                <div className="input-group">
+                  <select className="form-select" name="countryCode" value={formData.countryCode} onChange={handleChange} required>
+                    <option value="+91">🇮🇳 India +91</option>
+                    <option value="+1">🇺🇸 USA +1</option>
+                    <option value="+1">🇨🇦 Canada +1</option>
+                    <option value="+44">🇬🇧 UK +44</option>
+                  </select>
+                  <input type="tel" className="form-control" name="mobileNumber" value={formData.mobileNumber} onChange={handleChange} required />
+                </div>
+              </div>
+            </div>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Gender</label>
+              <div className="col-sm-4">
+                <select className="form-select" name="gender" value={formData.gender} onChange={handleChange} required>
+                  <option value="">Select Gender</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+              <label className="col-sm-2 col-form-label">Date of Birth</label>
+              <div className="col-sm-4">
+                <input type="date" className="form-control" name="dob" value={formData.dob} onChange={handleChange} required />
+              </div>
+            </div>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Nationality</label>
+              <div className="col-sm-4">
+                <input type="text" className="form-control" name="nationality" value={formData.nationality} onChange={handleChange} required />
+              </div>
+              <label className="col-sm-2 col-form-label">Country of Residence</label>
+              <div className="col-sm-4">
+                <select className="form-select" name="countryOfResidence" value={formData.countryOfResidence} onChange={handleChange} required>
+                  <option value="">Select Country</option>
+                  {topCountries.map((country) => (
+                    <option key={country} value={country}>{country}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <h2 className="mb-3">Passport Information</h2>
+            <hr/>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Passport Number</label>
+              <div className="col-sm-4">
+                <input type="text" className="form-control" name="passportNumber" value={formData.passportNumber} onChange={handleChange} required />
+              </div>
+              <label className="col-sm-2 col-form-label">Passport Issued Date</label>
+              <div className="col-sm-4">
+                <input type="date" className="form-control" name="passportIssuedDate" value={formData.passportIssuedDate} onChange={handleChange} />
+              </div>
+            </div>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Passport Expiry Date</label>
+              <div className="col-sm-4">
+                <input type="date" className="form-control" name="passportExpiryDate" value={formData.passportExpiryDate} onChange={handleChange} required />
+              </div>
+              <label className="col-sm-2 col-form-label">Passport Issued Country</label>
+              <div className="col-sm-4">
+                <select className="form-select" name="passportIssuedCountry" value={formData.passportIssuedCountry} onChange={handleChange} required>
+                  <option value="">Select Country</option>
+                  {topCountries.map((country) => (
+                    <option key={country} value={country}>{country}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <h2 className="mb-3">Documents Upload</h2>
+            <hr/>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Utility Bill</label>
+              <div className="col-sm-4">
+                <input type="file" className="form-control" name="utilityBill" onChange={handleChange} multiple />
+              </div>
+              <label className="col-sm-2 col-form-label">Photo</label>
+              <div className="col-sm-4">
+                <input type="file" className="form-control" name="photo" onChange={handleChange} multiple />
+              </div>
+            </div>
+
+            <div className="mb-3 row">
+              <label className="col-sm-2 col-form-label">Passport Scan</label>
+              <div className="col-sm-10">
+                <input type="file" className="form-control" name="passportScan" onChange={handleChange} multiple />
+              </div>
+            </div>
+
+            <div className="d-grid gap-2">
+              <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            </div>
+
+          </form>
         </div>
       </div>
+    </div>
+  </div>
+</div>
+
+
     </>
   );
 };
--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -1,84 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import Header from './Header/Header';
 
 const Faq = () => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [faqs, setFaqs] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0); // Set the first FAQ to be open by default
   const title = "FAQ";
   const descp = "Everything you need to know about Dubai Visa";
 
-  const faqs = [
-    {
-      question: 'What are the different types of visa?',
-      answer: 'The purpose of your visit will determine the type of visa you need to apply for. It could be a Dubai Tourist Visa (14/30/60 day, Regular, Express, and multi-entry Visa), transit visa.'
-    },
-    {
-      question: 'What should be the validity of my passport?',
-      answer: 'For Dubai Tourist Visa, the passport should be valid for at least six months from the date of travel.'
-    },
-    {
-      question: 'Is there an age criteria for applying UAE visa?',
-      answer: 'Yes, there is. Females below 18 years of age and males below 19 years of age need to apply for a UAE Visit Visa along with their father, mother, or husband to avoid any chances of rejection.'
-    },
-    {
-      question: 'What are the documents required for Dubai visa?',
-      answer: 'The documents required for a Dubai visa include a valid passport, completed visa application form, passport-size photographs, confirmed flight ticket, and proof of accommodation.'
-    },
-    {
-      question: 'How long does it take to process a Dubai visa?',
-      answer: 'The processing time for a Dubai visa varies depending on the type of visa and application method. It can range from 1 to 5 working days.'
-    },
-    {
-      question: 'Can I extend my Dubai visa?',
-      answer: 'Yes, you can extend your Dubai visa under certain circumstances. It is advisable to apply for an extension well in advance of your visa expiry date.'
-    },
-    {
-      question: 'What is the difference between a Dubai tourist visa and a visit visa?',
-      answer: 'A Dubai tourist visa is issued for tourism purposes and allows multiple entries, while a visit visa is intended for visiting family or friends and is usually for a single entry.'
-    },
-    {
-      question: 'Do I need travel insurance for a Dubai visa?',
-      answer: 'While travel insurance is not mandatory for obtaining a Dubai visa, it is highly recommended to protect against unexpected events such as medical emergencies, trip cancellations, or lost baggage.'
-    },
-    {
-      question: 'Can I work on a tourist visa in Dubai?',
-      answer: 'No, you cannot work on a tourist visa in Dubai. A separate employment visa is required to work legally in the UAE.'
-    },
-    {
-      question: 'What should I do if my Dubai visa application is rejected?',
-      answer: 'If your Dubai visa application is rejected, you can reapply after addressing the reasons for rejection or seek guidance from visa experts for assistance.'
-    },
-    {
-      question: 'Where can I get my international money exchanged?',
-      answer: 'Exchanging money in Dubai is not a tough task. Money exchange counters can be found in banks, shopping centres, and malls where you can exchange your foreign currency. However, the majority of banks and ATMs also accept credit cards.'
-    },
-    {
-      question: 'What is Dubai\'s weather forecast?',
-      answer: 'Because Dubai is located in the Arabian Desert, its climate is primarily hot and muggy. The temperature may climb to 40 to 45 degrees Celsius in the summer, however, in the winter, when the temperature falls to 25 degrees Celsius or below, particularly between October to March, it feels nice, cold, and refreshing.'
-    },
-    {
-      question: 'What is UAE\'s time zone?',
-      answer: 'The time zone of UAE is GMT/UTC +4 hours.'
-    },
-    {
-      question: 'Which type of telecommunication services is provided in the UAE?',
-      answer: 'Du and Etisalat are the main mobile phone providers in Dubai.'
-    },
-    {
-      question: 'What is the official currency of the UAE?',
-      answer: 'The official currency of UAE is the Emirates dirham (AED).'
-    },
-    {
-      question: 'How much cash should people travelling to the UAE bring with them?',
-      answer: 'When visiting the UAE as a tourist, you must have a return ticket and hotel reservation in addition to a minimum of 3000 AED in cash or credit card.'
-    },
-    {
-      question: 'Can single women travel to the UAE as tourists?',
-      answer: 'Yes, single women can get a 30-day tourist visa, renewable once for a similar period. They must be sponsored by a tourist agency or hotel. Note that visa applications for women under 25 may have a higher chance of rejection.'
-    }
-  ];
+  const username1 = 'admin';
+  const password1 = '1234';
+  const encodedCredentials = 'Basic ' + btoa(username1 + ':' + password1);
+  const headers = {
+    'Authorization': encodedCredentials,
+    'X-API-KEY': 'CODEX@123',
+  };
+
+  useEffect(() => {
+    // Fetch FAQs from your CodeIgniter API
+    axios.get('https://thesoftwareexperts.com/cdksolar/admin/api/faq_all', { headers })
+      .then(response => {
+        setFaqs(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching FAQs:', error);
+      });
+  }, []);
 
   const toggleFAQ = (index) => {
     setActiveIndex(activeIndex === index ? null : index);
@@ -123,7 +73,7 @@ const Faq = () => {
                   </div>
                   <div
                     id={`collapse${index}`}
-                    className={`collapse ${activeIndex === index ? 'show' : ''}`}
+                    className={`collapse ${activeIndex === index || (activeIndex === 0 && index === 0) ? 'show' : ''}`}
                     aria-labelledby={`heading${index}`}
                     data-parent="#faqAccordion"
                   >
